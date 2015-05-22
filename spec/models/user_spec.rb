e989e2f9c0c1ecb41b821cdb28299f71b9a34ec4
Kazio <mr.coldhand@gmail.com>
2015-05-22 22:45:15 +0200

    wrote my own test for validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,25 @@ describe User do
   it { should validate_presence_of :firstname }
   it { should validate_presence_of :lastname }
 
-  it "by default isn't admin" do
+  describe "presence of lastname validation" do
+  	context "when lastname is not given" do
+  		it "raises an error" do
+  			skip
+				user = User.create # create <=> User.new.save
+				expect(user.errors[:lastname]).not_to be_empty
+  		end
+  	end
+
+  	context "when lastname is given" do
+  		it "does not raise any error" do
+  			skip
+  			user = User.create(lastname: 'Kulfon')
+				expect(user.errors[:lastname]).to be_empty
+  		end
+  	end
+  end
+
+  it "by default is not admin" do
     expect(User.new).not_to be_admin
   end
 
