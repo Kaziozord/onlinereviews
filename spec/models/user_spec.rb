182de4c51418796f5fda29d086e000670fcb0517
Kazio <mr.coldhand@gmail.com>
2015-05-22 21:43:05 +0200

    1st issue solved

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,10 @@ describe User do
   it { should validate_presence_of :lastname }
 
   it "by default isn't admin" do
-    expect(User.new).to_not be_admin
+    expect(User.new).not_to be_admin
+  end
+
+  it "is admin" do
+  	expect(User.new(admin: true)).to be_admin
   end
 end
