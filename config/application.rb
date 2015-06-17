faa6b589de182660709463461dac2955c3b3f7b7
Kazio <mr.coldhand@gmail.com>
2015-06-17 21:36:09 +0200

    static assets enabled

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,12 @@ module Workshops
   end
 end
 
-ActionDispatch::Callbacks.after do
-  # Reload the factories
-  return unless (Rails.env.development? || Rails.env.test?)
+# ActionDispatch::Callbacks.after do
+#   # Reload the factories
+#   return unless (Rails.env.development? || Rails.env.test?)
 
-  unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
-    FactoryGirl.factories.clear
-    FactoryGirl.find_definitions
-  end
-end
+#   unless FactoryGirl.factories.blank? # first init will load factories, this should only run on subsequent reloads
+#     FactoryGirl.factories.clear
+#     FactoryGirl.find_definitions
+#   end
+# end
